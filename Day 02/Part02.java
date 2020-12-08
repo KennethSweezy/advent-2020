351ecfa5273b0f9fa002d511af0569ab232d9cf4
KennethSweezy <KennethSweezy@gmail.com>
2020-12-08 10:24:57 -0500

    Day 02, Part 02

---
+++ Day 02/Part02.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Part02 {
+
+	public static void main(String[] args) {
+		ArrayList lines = new ArrayList<String>();
+		try {
+			File input = new File("input/input.txt");
+			Scanner scan = new Scanner(input);
+			while(scan.hasNextLine()) {
+				lines.add(scan.nextLine());
+			}
+			String[] arr;
+			String[] tempIntarr;
+			int[] allowedNum = new int[2];
+			String allowedChar;
+			int validPasses = 0;
+			for(int i = 0; i < lines.size(); i++) {
+				arr = ((String) lines.get(i)).split(" ", 3);
+				tempIntarr = arr[0].split("-", 2);
+				allowedNum[0] = Integer.parseInt(tempIntarr[0]);
+				allowedNum[1] = Integer.parseInt(tempIntarr[1]);
+				allowedChar = arr[1].substring(0, 1);
+				System.out.print(lines.get(i) + " " + (arr[2].length() - arr[2].replace(allowedChar, "").length()));
+				char allChar = allowedChar.charAt(0);
+				if(arr[2].charAt(allowedNum[0] - 1) == allChar ? (arr[2].charAt(allowedNum[1] - 1) == allChar ? false : true) : arr[2].charAt(allowedNum[1] - 1) == allChar) {
+					validPasses++;
+					System.out.print(" VALID!\n");
+				} else {
+					System.out.print("\n");
+				}
+			}
+			System.out.println(validPasses);
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Oops, something went wrong.");
+			e.printStackTrace();
+		}
+	}
+}
